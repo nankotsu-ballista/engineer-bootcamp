5f0776d2496723093c38ef9618d2de36f9374d60
kaede3128739 <69568626+nankotsu-ballista@users.noreply.github.com>
2025-06-03 05:23:50 +0900

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,10 @@
+from typing import List
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            pair = target - num
+            if pair in seen:
+                return [seen[pair], i]
+            seen[num] = i
